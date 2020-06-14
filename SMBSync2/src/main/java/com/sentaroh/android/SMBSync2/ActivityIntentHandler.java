1934f61fb9d3b18890f84f7e95435c61e4ba1857
Sentaroh <gm.developer.fhoshino@gmail.com>
2020-06-14 09:12:38 +0900

    Prevent java.lang.IllegalStateException.

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityIntentHandler.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityIntentHandler.java
@@ -1,16 +1,16 @@
 package com.sentaroh.android.SMBSync2;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
-import com.sentaroh.android.Utilities.Dialog.MessageDialogFragment;
+import com.sentaroh.android.Utilities.Dialog.MessageDialogAppFragment;
+import com.sentaroh.android.Utilities.NotifyEvent;
 
-public class ActivityIntentHandler extends AppCompatActivity {
+public class ActivityIntentHandler extends Activity {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(new GlobalParameters().setNewLocale(base, false));
@@ -21,22 +21,36 @@ public class ActivityIntentHandler extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_transrucent);
-
+        final Context c=ActivityIntentHandler.this;
         Intent received_intent=getIntent();
         if (received_intent.getAction()!=null && !received_intent.getAction().equals("")) {
             Intent in=new Intent(received_intent.getAction());
             in.setClass(ActivityIntentHandler.this, SyncService.class);
             if (received_intent.getExtras() != null) in.putExtras(received_intent.getExtras());
-            final FragmentManager fm=getSupportFragmentManager();
             try {
-                startService(in);
-            }catch(Exception e){
-                MessageDialogFragment mdf=MessageDialogFragment.newInstance(false, "ActivityIntentHandler start service error", e.getMessage(), null);
-                mdf.showDialog(fm, mdf, null);
-            }
-        }
+                c.startService(in);
+                finish();
+            } catch(Exception e) {
+                e.printStackTrace();
+                NotifyEvent ntfy=new NotifyEvent(c);
+                ntfy.setListener(new NotifyEvent.NotifyEventListener() {
+                    @Override
+                    public void positiveResponse(Context context, Object[] objects) {
+                        finish();
+                    }
 
-        finish();
+                    @Override
+                    public void negativeResponse(Context context, Object[] objects) {
+
+                    }
+                });
+                final FragmentManager fm=getFragmentManager();
+                MessageDialogAppFragment mdf=MessageDialogAppFragment.newInstance(false, "E",
+                        "SMBSync2", "ShortcutAutoSync start service error\n"+e.getMessage());
+                mdf.showDialog(fm, mdf, ntfy);
+            }
+
+        }
     }
 
 }
